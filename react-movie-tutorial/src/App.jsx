8a981ee72215c4758014594b71d15ed4a78c4bf2
Rahman2782 <rahman2782@gmail.com>
2025-07-07 03:33:25 -0400

    learning react - initial commit

---
+++ react-movie-tutorial/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css"
+import Home from "./pages/Home";
+
+// //components return HTML
+// export default function App() { //components start with a capital letter
+//   return ( 
+//     <>
+//       <Text display={"whats up"}/>
+//       <Text display={"gurt"}/>
+//     </> //only one parent element can be returned unless fragments are used
+//   )
+// }
+
+// function Text({display}) {
+//   return (
+//     <div>
+//       <p>{display}</p>
+//     </div>
+//   )
+// }
+
+function App() {
+  const movieNumber = 1;
+
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ react-movie-tutorial/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import MovieCard from "../components/MovieCard"
+
+export default function Home () {
+    const movies = [
+        {id: 1, title: "Madame Web", release_date: "2024"},
+        {id: 2, title: "Tall Girl", release_date: "2018"},
+        {id: 3, title: "Tall Girl 2", release_date: "2020"},
+        {id: 4, title: "Hurry Up Tommorow", release_date: "2025"},
+        {id: 5, title: "Toy Story 2", release_date: "2001"},
+    ];
+
+    const handleSearch = () => {
+
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search" 
+                    className="search-input"
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id}/>
+                ))}
+            </div>
+        </div>
+    )
+}
