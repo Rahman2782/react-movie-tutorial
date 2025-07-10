e68ab0aacbe25813eb043791c4496358e2cd1534
Rahman2782 <rahman2782@gmail.com>
2025-07-10 02:51:49 -0400

    added provided css from tutorial, also added page routing

--- react-movie-tutorial/src/App.jsx
+++ react-movie-tutorial/src/App.jsx
@@ -1,5 +1,8 @@
-import "./App.css"
+import "./css/App.css"
+import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
 
 // //components return HTML
 // export default function App() { //components start with a capital letter
@@ -21,9 +24,15 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favourites" element={<Favourites />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ react-movie-tutorial/src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favourites.css"
+
+export default function Favourites() {
+    return (
+        <div className="favourites-empty">
+            <h2>No favourite movies yet</h2>
+            <p>Start adding movies to your favourites and they will appear here</p>
+        </div>
+    )
+}
